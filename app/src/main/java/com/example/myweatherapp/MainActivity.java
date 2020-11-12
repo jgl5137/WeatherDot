e8c3f7d93f01f3ec06622d576b31b8edf157be0d
Joey Lau <jgl5137@psu.edu>
2020-11-12 00:44:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+    }
+}
